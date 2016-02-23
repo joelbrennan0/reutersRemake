6a55ffd80fa7f67cde991a5b521aed47d6796e53
Joel Brennan <joel.brennan@tpagency.com>
2016-02-23 14:44:41 +0000

    commit few tweaks before attempting submodule deployment

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
+$('.in-the-news .press-link-container').hide();
+$('.in-the-news .press-link-container').slice(0,6).show();
 
+function handler() {
+  $('.in-the-news .press-link-container').animate().show();
+  $('body,html').animate({
+    scrollTop: $('.in-the-news .links-container div:nth-child(7)').offset().top -5
+  }, 500);
+  $('#seeMore').remove();
+}
+$('.in-the-news .links-container').each(function() {
+  var link = $('<a id="seeMore">SEE MORE NEWS</a>');
+  $(this).append(link);
+  link.click(handler);
+});
